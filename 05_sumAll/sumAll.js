7db833a3e72270aa823ed0faef05d5e96dd44b4e
bugthug <avivyodfat@proton.me>
2023-04-25 08:28:20 +0300

    Solve challenge 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(bottom, top) {
+    console.log(typeof bottom);
+    if (typeof bottom !== 'number' || typeof top !== 'number') {
+        return 'ERROR';
+    }
 
+    if (bottom < 0 || top < 0) {
+        return 'ERROR';
+    }
+
+    if (bottom > top) {
+        let temp1 = bottom;
+        bottom = top;
+        top = temp1;
+    }
+
+    let sum = 0;
+
+    for (let currentIndex = bottom; currentIndex <= top; currentIndex++) {
+        sum += currentIndex;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
