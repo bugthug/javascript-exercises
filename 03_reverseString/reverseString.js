78e2112123de982e3a7dda0669ba0192e3278c02
bugthug <avivyodfat@proton.me>
2023-04-25 07:36:56 +0300

    Solve challenge 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = Array.from(string);
 
+    let stringText = stringArray.reverse().toString()
+    // we must remove ',' as they come included from Array.prototype.toString()
+    stringText = stringText.replaceAll(',',"")
+
+    return stringText;
 };
 
 // Do not edit below this line
