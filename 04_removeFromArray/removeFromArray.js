f21c4fd0d5076aa48b125bdb502d70e694c2e371
bugthug <avivyodfat@proton.me>
2023-04-25 08:02:44 +0300

    Solve challenge 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...items) {
+    for (let i = 0; i < items.length; i++){
+        array.filter(function(value,index,arr) {
+            if (value===items[i]) {
+                arr.splice(index,1);
+            }
+        })
+    }
+    return array;
 };
 
 // Do not edit below this line
